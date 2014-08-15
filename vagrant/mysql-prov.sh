4a6e4443b68750706f60dd8a2591c92f31219e62
enqae <enqae@19drops.com>
2014-08-15 10:55:20 +0200

    now yes mysql removed

--- vagrant/mysql-prov.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo mysql-server mysql-server/root_password password wowwow | sudo debconf-set-selections
-echo mysql-server mysql-server/root_password_again password wowwow | sudo debconf-set-selections
-
-# mysqladmin -u root password mysecretpasswordgoeshere
-
-sudo apt-get -y install mysql-server-$1
-
