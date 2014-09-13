470db3091ed17c3e08718fa7a3f722b8793de63f
enqae <enqae@19drops.com>
2014-09-13 12:06:13 +0200

    Ranking Retriever - Part V

--- app/app/com/plyrhub/ranking/service/MemberScorer.scala
+++ app/app/com/plyrhub/ranking/service/MemberScorer.scala
@@ -84,7 +84,8 @@ class MemberScorer extends Actor with ActorLogging {
     // Fail ->>> Send to MisterWolf
 
     // Save score for rankings
-    val fScoreForRankings = RankingRepo.saveScoreForRankings(owner, member, rankings, score, uniqueRepoId)
+    //val fScoreForRankings = RankingRepo.saveScoreForRankings(owner, member, rankings, score, uniqueRepoId)
+    val fScoreForRankings = Future.successful(SimpleSuccess())
 
     // Look for the provided rankings to see in they exist
     val fRankingsVerification = RankingRepo.verifyRankingsOnMember(owner, member, rankings)
