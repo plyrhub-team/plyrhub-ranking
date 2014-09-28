ebca6fdd1332af833dfb5bfbcf4ec994fc3893d7
enqae <enqae@19drops.com>
2014-09-28 12:49:29 +0200

    Ranking Retriever - Part n-002

--- app/app/com/plyrhub/ranking/service/RedisScorer.scala
+++ app/app/com/plyrhub/ranking/service/RedisScorer.scala
@@ -73,10 +73,10 @@ class RedisScorer extends Actor with ActorLogging {
   def manageRedisSuccess(owner: String, member: String, ranking: String, opId: String) = {
 
     // It was annotated, so tell Mongo it was done to pull it out from the reconstruction process
-    //RankingRepo
-    //  .commitScoreForRanking(owner, member, ranking, opId)
-    //  .map(result =>
-    //  result.fold(manageMongoFailure, manageMongoSuccess))
+    RankingRepo
+      .commitScoreForRanking(owner, member, ranking, opId)
+      .map(result =>
+      result.fold(manageMongoFailure, manageMongoSuccess))
   }
 
   def manageMongoFailure(f:ServiceFailure) = {
